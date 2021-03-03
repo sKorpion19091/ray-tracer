edde7e7f8e37c034be3479da512f9d448a052a37
Kyle Smith <sKorpion19091>
2021-03-03 14:03:46 +0200

    First Commit

---
+++ makefile
@@ -0,0 +1,10 @@
+run:
+	g++ -std=c++11 ./src/*.cpp -I ./include -o main
+	./main > ./res/image.ppm
+
+open-image:
+	open ./res/image.ppm
+
+create-image:
+	./main > ./res/image.ppm
+	open ./res/image.ppm
---
+++ src/Main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "../include/Vec3.h"
+#include "../include/Ray.h"
+#include "../include/Vec3Utils.h"
+#include "../include/Color.h"
+
+using namespace Vec3Utils;
+
+Color ray_color(const Ray& r) {
+    Vec3 unit_dir = unit_vector(r.direction());
+    auto t = 0.5 * (unit_dir.y() + 1.0);
+    return (1.0 - t) * Color(1.0,1.0,1.0) + t * Color(0.5,0.7,1.0);
+}
+
+int main() {
+
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto viewport_height = 4.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+    auto origin = Point3(2, 0, 0);
+    auto horizontal = Vec3(viewport_width, 0, 0);
+    auto vertical = Vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length);
+
+    // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            Ray r(origin, lower_left_corner + u * horizontal + v*vertical - origin);
+            Color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
+    }
+    std::cerr << "\nDone.\n";
+}
